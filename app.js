26504456aa6d17451128e5b3c59299cd1345946a
parnexcodes <parnexcodes@gmail.com>
2022-11-24 00:37:30 +0530

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fastify = require('fastify')({ logger: true })
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+  return { hello: 'hello' }
 })
 
 // Run the server!
